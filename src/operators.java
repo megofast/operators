da9125e56ac7b924542db241b2f4770194569dd7
megofast <33850107+megofast@users.noreply.github.com>
2018-01-28 11:55:25 -0600

    Initial commit

---
+++ src/operators.java
@@ -0,0 +1,9 @@
+public class operators {
+    public static void main(String args[]) {
+        System.out.println("2 + 4 = " + (2 + 4));
+        System.out.println("7 - 4 = " + (7 - 4));
+        System.out.println("9 * 4 = " + (9 * 4));
+        System.out.println("8 / 2 = " + (8 / 2));
+        System.out.println("5 % 4 = " + (5 % 4));
+    }
+}
